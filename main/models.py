c0fa7dc78c7d76585d8bea79426b73ca4d547691
adeleke-code <femiadeleke601@gmail.com>
2023-11-06 22:07:16 +0100

    initial setup

--- main/models.py
+++ main/models.py
@@ -111,8 +111,21 @@ class Datasets(models.Model):
     
 
 
+class Tags(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=650)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
+    def __str__(self):
+        return self.name
+    
 
-
-
+    def delete(self):
+        self.is_deleted = True
+        self.save()
+        
+    
