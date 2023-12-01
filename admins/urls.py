36ea9996487a3831c5caf485918d955009fc0863
adeleke-code <femiadeleke601@gmail.com>
2023-12-01 16:10:38 +0100

    admin actions

--- admins/urls.py
+++ admins/urls.py
@@ -9,5 +9,7 @@ from .views import *
 
 
 urlpatterns = [
+    path('admin/datatsets/', AdminDatatsetView.as_view(), name="admin_datatsets"),
+    path('admin/datatsets/pk/<uuid:pk>/actions/<str:action>/', dataset_actions, name="admin_datatsets_actions"),
     
 ]
